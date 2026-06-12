7d11118736a4e48972dd049bc78fc4449260c032
Windy <windynapitupulu3108@gmail.com>
2026-06-12 07:59:19 +0700

    Initial Commit

---
+++ Jigap/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Jigap
+//
+//  Created by Windy Claudia Napitupulu on 12/06/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Jigap/JigapApp.swift
@@ -0,0 +1,17 @@
+//
+//  JigapApp.swift
+//  Jigap
+//
+//  Created by Windy Claudia Napitupulu on 12/06/26.
+//
+
+import SwiftUI
+
+@main
+struct JigapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
